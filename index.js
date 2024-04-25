2ce6006219db142b88eb9b4df40a80dad2b9570b
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-25 07:36:45 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+// Start with your work from Exercise 40.
+// Call the function make_great() with a copy of the array of magicians’ names.
+// Because the original array will be unchanged, return the new array and store it in a separate array.
+// Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+let magicain = ["Harry Potter", "Hermione Granger", "Ron weasley", "Albus Dumbledore"];
+function newArray(arr) {
+    return [...arr];
+}
+function make_great(magicainArray) {
+    for (let i = 0; i < magicainArray.length; i++) {
+        magicainArray[i] = "The Great " + magicainArray[i];
+    }
+}
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+// calling fucntions
+const copyArray = newArray(magicain);
+make_great(copyArray);
+console.log("\n\nThis is my orignal array:");
+show_magicians(magicain);
+console.log("\n\nThis is my copy array:");
+show_magicians(copyArray);
